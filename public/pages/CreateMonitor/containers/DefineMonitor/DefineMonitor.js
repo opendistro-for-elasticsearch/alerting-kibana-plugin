d58443a6cbb50d69d0c6f0615f0a921cfebc986a
Tianli Feng <ftianli@amazon.com>
2020-06-24 22:31:33 -0700

    merge onRunHttp into onRunQuery in DefineMonitor.js

--- public/pages/CreateMonitor/containers/DefineMonitor/DefineMonitor.js
+++ public/pages/CreateMonitor/containers/DefineMonitor/DefineMonitor.js
@@ -70,7 +70,6 @@ class DefineMonitor extends Component {
 
     this.renderGraph = this.renderGraph.bind(this);
     this.onRunQuery = this.onRunQuery.bind(this);
-    this.onRunHttp = this.onRunHttp.bind(this);
     this.resetResponse = this.resetResponse.bind(this);
     this.onQueryMappings = this.onQueryMappings.bind(this);
     this.queryMappings = this.queryMappings.bind(this);
@@ -172,24 +171,36 @@ class DefineMonitor extends Component {
     const { httpClient, values } = this.props;
     const formikSnapshot = _.cloneDeep(values);
 
-    // If we are running a visual graph query, then we need to run two separate queries
-    // 1. The actual query that will be saved on the monitor, to get accurate query performance stats
-    // 2. The UI generated query that gets [BUCKET_COUNT] times the aggregated buckets to show past history of query
-    // If the query is an extraction query, we can use the same query for results and query performance
-    const searchRequests = [buildSearchRequest(values)];
-    if (values.searchType === SEARCH_TYPE.GRAPH) {
-      searchRequests.push(buildSearchRequest(values, false));
+    const searchType = values.searchType;
+    let generalRequests;
+    switch (searchType) {
+      case SEARCH_TYPE.QUERY:
+        generalRequests = [buildSearchRequest(values)];
+        break;
+      case SEARCH_TYPE.GRAPH:
+        // If we are running a visual graph query, then we need to run two separate queries
+        // 1. The actual query that will be saved on the monitor, to get accurate query performance stats
+        // 2. The UI generated query that gets [BUCKET_COUNT] times the aggregated buckets to show past history of query
+        // If the query is an extraction query, we can use the same query for results and query performance
+        generalRequests = [buildSearchRequest(values)];
+        generalRequests.push(buildSearchRequest(values, false));
+        break;
+      case SEARCH_TYPE.HTTP:
+        generalRequests = [buildHTTPRequest(values)];
+        break;
     }
 
     try {
-      const promises = searchRequests.map((searchRequest) => {
+      const promises = generalRequests.map((generalRequest) => {
         // Fill in monitor name in case it's empty (in create workflow)
         // Set triggers to empty array so they are not executed (if in edit workflow)
         // Set input search to query/graph query and then use execute API to fill in period_start/period_end
         const monitor = formikToMonitor(values);
         _.set(monitor, 'name', 'TEMP_MONITOR');
         _.set(monitor, 'triggers', []);
-        _.set(monitor, 'inputs[0].search', searchRequest);
+        if (searchType === SEARCH_TYPE.QUERY || searchType === SEARCH_TYPE.HTTP)
+          _.set(monitor, 'inputs[0].search', generalRequest);
+        else if (searchType === SEARCH_TYPE.HTTP) _.set(monitor, 'inputs[0].http', generalRequest);
         return httpClient.post('../api/alerting/monitors/_execute', monitor);
       });
 
@@ -204,41 +215,6 @@ class DefineMonitor extends Component {
         this.setState({ response, formikSnapshot, performanceResponse });
       } else {
         console.error('There was an error running the query', queryResponse.data.resp);
-        this.setState({ response: null, formikSnapshot: null, performanceResponse: null });
-      }
-    } catch (err) {
-      console.error('There was an error running the query', err);
-    }
-  }
-
-  async onRunHttp() {
-    const { httpClient, values } = this.props;
-    const formikSnapshot = _.cloneDeep(values);
-
-    const httpRequests = [buildHTTPRequest(values)];
-    try {
-      const promises = httpRequests.map((httpRequest) => {
-        // Fill in monitor name in case it's empty (in create workflow)
-        // Set triggers to empty array so they are not executed (if in edit workflow)
-        // Set input search to query/graph query and then use execute API to fill in period_start/period_end
-        const monitor = formikToMonitor(values);
-        _.set(monitor, 'name', 'TEMP_MONITOR');
-        _.set(monitor, 'triggers', []);
-        _.set(monitor, 'inputs[0].http', httpRequest);
-        return httpClient.post('../api/alerting/monitors/_execute', monitor);
-      });
-
-      const [httpResponse, optionalResponse] = await Promise.all(promises);
-
-      if (httpResponse.data.ok) {
-        const response = _.get(httpResponse.data.resp, 'input_results.results[0]');
-        // If there is an optionalResponse use it's results, otherwise use the original response
-        const performanceResponse = optionalResponse
-          ? _.get(optionalResponse, 'data.resp.input_results.results[0]', null)
-          : response;
-        this.setState({ response, formikSnapshot, performanceResponse });
-      } else {
-        console.error('There was an error running the query', httpResponse.data.resp);
         this.setState({ response: null, formikSnapshot: null, performanceResponse: null });
       }
     } catch (err) {
@@ -322,7 +298,7 @@ class DefineMonitor extends Component {
     }
     return {
       actions: [
-        <EuiButton disabled={runIsDisabled} onClick={this.onRunQuery}>
+        <EuiButton disabled={runIsDisabled} onClick={this.onRunRequest}>
           Run
         </EuiButton>,
       ],
