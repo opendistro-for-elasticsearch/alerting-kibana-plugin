e703a61997040d89eb76eda5a7222114436e58df
Tianli Feng <ftianli@amazon.com>
2020-07-18 22:41:55 -0700

    auto focus the first invalid field

--- public/utils/SubmitErrorHandler.js
+++ public/utils/SubmitErrorHandler.js
@@ -14,18 +14,25 @@
  */
 
 import { useEffect } from 'react';
-// This function is used to:
-// 1. call function 'onSubmitError()' when validation fails on submit
-// 2. auto scroll the page to the first field with error
-// Reference: https://github.com/formium/formik/issues/1484 and https://github.com/formium/formik/issues/146
+/* This function is used to:
+ 1. call function 'onSubmitError()' when validation fails on submit
+ 2. auto scroll the page to the first field with error, and focus the field
+ Reference: https://github.com/formium/formik/issues/1484 and https://github.com/formium/formik/issues/146 */
 export const SubmitErrorHandler = (props) => {
   const errorKeys = Object.keys(props.errors);
   const effect = () => {
     if (errorKeys.length > 0 && !props.isSubmitting && !props.isValid) {
       props.onSubmitError();
-      const selector = `[name="${errorKeys[0]}"]`;
-      const errorElement = document.querySelector(selector);
-      if (errorElement) errorElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      /* Use 2 selectors to locate the input field. Because some <input> elements only have 'name' attribute,
+      some <input> only have 'id', but are nested in a <div> with same value in 'name' attribute. */
+      const idSelector = `[id="${errorKeys[0]}"]`;
+      const nameSelector = `[name="${errorKeys[0]}"]`;
+      const errorElement =
+        document.querySelector(idSelector) ?? document.querySelector(nameSelector);
+      if (errorElement) {
+        errorElement.focus();
+        errorElement.scrollIntoView({ behavior: 'smooth', block: 'center' });
+      }
     }
   };
   useEffect(effect, [props.isSubmitting]);
