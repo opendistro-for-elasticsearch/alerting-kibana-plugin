bf67db07b210bf4b4ffd862759f7592e6c476fd2
Tianli Feng <ftianli@amazon.com>
2020-07-20 10:38:12 -0700

    downgrade 'Monitor state' text from subheader to label in bold text

--- public/pages/CreateMonitor/components/MonitorState/MonitorState.js
+++ public/pages/CreateMonitor/components/MonitorState/MonitorState.js
@@ -16,20 +16,19 @@
 import React, { Fragment } from 'react';
 import { EuiSpacer } from '@elastic/eui';
 
-import SubHeader from '../../../../components/SubHeader';
 import { FormikCheckbox } from '../../../../components/FormControls';
 
 const MonitorState = () => (
   <Fragment>
-    <SubHeader
-      title={<h4>Monitor state</h4>}
-      description={<span>Disabled monitors do not run.</span>}
-    />
     <EuiSpacer size="s" />
     <FormikCheckbox
       name="disabled"
       formRow
-      rowProps={{ style: { paddingLeft: '10px' } }}
+      rowProps={{
+        label: 'Monitor state',
+        style: { paddingLeft: '10px' },
+        helpText: 'Disabled monitors do not run.',
+      }}
       inputProps={{ label: 'Disable monitor' }}
     />
   </Fragment>
