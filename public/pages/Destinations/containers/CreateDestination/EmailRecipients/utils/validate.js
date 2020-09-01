daea388555a06aaca662459b22fa4e5fee863796
Mohammad Qureshi <qreshi@amazon.com>
2020-09-01 11:48:35 -0700

    Update validateEmailRecipients to return all invalid email entries

--- public/pages/Destinations/containers/CreateDestination/EmailRecipients/utils/validate.js
+++ public/pages/Destinations/containers/CreateDestination/EmailRecipients/utils/validate.js
@@ -17,12 +17,15 @@ import _ from 'lodash';
 import { RECIPIENT_TYPE } from './constants';
 import { isValidEmail } from '../../../../components/createDestinations/Email/utils/validate';
 
-export const validateEmailRecipients = options => {
+export const validateEmailRecipients = (options) => {
   if (_.isEmpty(options)) return 'Required';
 
+  let invalidEmails = [];
   for (const option of options) {
     if (option.type === RECIPIENT_TYPE.EMAIL && !isValidEmail(option.value)) {
-      return 'At least one of the specified emails is invalid';
+      invalidEmails.push(option.value);
     }
   }
+
+  if (invalidEmails.length > 0) return `Invalid emails: ${invalidEmails}`;
 };
