59fcb859c626cfd5471b6654a92c65381aaf8b30
Mohammad Qureshi <qreshi@amazon.com>
2020-09-13 23:53:13 -0700

    Make email verification RFC 5322 compliant

--- public/pages/Destinations/components/createDestinations/Email/utils/validate.js
+++ public/pages/Destinations/components/createDestinations/Email/utils/validate.js
@@ -64,4 +64,10 @@ export const validatePort = (value) => {
   if (!value) return 'Required';
 };
 
-export const isValidEmail = (value) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value);
+/**
+ * Validating email against a RFC 5322 (https://www.ietf.org/rfc/rfc5322.txt) compliant pattern
+ */
+export const isValidEmail = (value) =>
+  /^(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/i.test(
+    value
+  );
