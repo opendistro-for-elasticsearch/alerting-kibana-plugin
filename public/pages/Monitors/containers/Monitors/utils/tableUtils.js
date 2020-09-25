aad8faff07366a1138b789d740d4050eb4c954f7
Tianli Feng <ftianli@amazon.com>
2020-09-24 22:22:12 -0700

    add comments to explain all the cases of the user object

--- public/pages/Monitors/containers/Monitors/utils/tableUtils.js
+++ public/pages/Monitors/containers/Monitors/utils/tableUtils.js
@@ -42,6 +42,12 @@ export const columns = [
     truncateText: true,
     textOnly: true,
     width: '100px',
+    /* There are 3 cases:
+    1. Monitors created by older versions and never updated.
+       These monitors won’t have User details in the monitor object. `monitor.user` will be null.
+    2. Monitors are created when security plugin is disabled, these will have empty User object.
+       (`monitor.user.name`, `monitor.user.roles` are empty )
+    3. Monitors are created when security plugin is enabled, these will have an User object. */
     render: (_, item) =>
       item.monitor.user && item.monitor.user.name ? item.monitor.user.name : 'N/A',
   },
