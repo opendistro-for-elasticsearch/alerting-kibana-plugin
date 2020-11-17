46b9c9165e0e29d3dcffe30218276d237b1837b1
Tianli Feng <ftianli@amazon.com>
2020-11-17 15:40:45 -0800

    cherry-pick PR#203

--- server/services/MonitorService.js
+++ server/services/MonitorService.js
@@ -424,21 +424,4 @@ export default class MonitorService {
       });
     }
   };
-
-  //TODO: This is temporarily a pass through call which needs to be deprecated
-  searchMonitors = async (req, h) => {
-    try {
-      const { query, index, size } = req.payload;
-      const params = { index, size, body: query };
-
-      const { callWithRequest: alertingCallWithRequest } = await this.esDriver.getCluster(
-        CLUSTER.ALERTING
-      );
-      const results = await alertingCallWithRequest(req, 'alerting.getMonitors', params);
-      return { ok: true, resp: results };
-    } catch (err) {
-      console.error('Alerting - MonitorService - searchMonitor:', err);
-      return { ok: false, resp: err.message };
-    }
-  };
 }
