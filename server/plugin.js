fa65396c1b21fb7ffa0832d0fea75954ebec5895
Tianli Feng <ftianli@amazon.com>
2020-11-17 16:27:43 -0800

    Add copyright header for the newly created files

--- server/plugin.js
+++ server/plugin.js
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2020 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License").
+ *   You may not use this file except in compliance with the License.
+ *   A copy of the License is located at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   or in the "license" file accompanying this file. This file is distributed
+ *   on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ *   express or implied. See the License for the specific language governing
+ *   permissions and limitations under the License.
+ */
+
 import { first } from 'rxjs/operators';
 import { createAlertingCluster, createAlertingADCluster } from './clusters';
 import {
@@ -16,6 +31,7 @@ export class AlertingPlugin {
   }
 
   async setup(core) {
+    // Get the global configuration settings of the cluster
     const globalConfig = await this.globalConfig$.pipe(first()).toPromise();
 
     // Create clusters
@@ -36,7 +52,7 @@ export class AlertingPlugin {
       anomalyDetectorService,
     };
 
-    // create router
+    // Create router
     const router = core.http.createRouter();
     // Add server routes
     alerts(services, router);
