707f20cfc8e8a190828a7801e6a88d1580027273
Tianli Feng <ftianli@amazon.com>
2020-11-24 14:46:51 -0800

    create a constant for query result size used in select components (#213)
    (cherry picked from commit f4ee301c274e6e1d519d52131210d834f7e80654)

--- public/pages/Destinations/containers/CreateDestination/EmailRecipients/utils/helpers.js
+++ public/pages/Destinations/containers/CreateDestination/EmailRecipients/utils/helpers.js
@@ -13,10 +13,12 @@
  *   permissions and limitations under the License.
  */
 
+import { MAX_QUERY_RESULT_SIZE } from '../../../../../../utils/constants';
+
 export default async function getEmailGroups(httpClient, searchText = '') {
   try {
     const response = await httpClient.get('../api/alerting/destinations/email_groups', {
-      query: { search: searchText, size: 200 },
+      query: { search: searchText, size: MAX_QUERY_RESULT_SIZE },
     });
     if (response.ok) {
       return response.emailGroups;
