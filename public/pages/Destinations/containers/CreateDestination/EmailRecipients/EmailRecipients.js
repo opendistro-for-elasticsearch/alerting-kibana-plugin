837373871487968bec3da6be546f949742217be4
Tianli Feng <ftianli@amazon.com>
2020-12-08 13:38:33 -0800

    Revert passing core.notifications to Email componenet as made in another PR

--- public/pages/Destinations/containers/CreateDestination/EmailRecipients/EmailRecipients.js
+++ public/pages/Destinations/containers/CreateDestination/EmailRecipients/EmailRecipients.js
@@ -101,7 +101,7 @@ export default class EmailRecipients extends React.Component {
   };
 
   render() {
-    const { httpClient, type, notifications } = this.props;
+    const { httpClient, type } = this.props;
     const { recipientOptions, isLoading, showManageEmailGroupsModal } = this.state;
     return (
       <Fragment>
@@ -159,7 +159,6 @@ export default class EmailRecipients extends React.Component {
           isVisible={showManageEmailGroupsModal}
           onClickCancel={this.onClickCancel}
           onClickSave={this.onClickSave}
-          notifications={notifications}
         />
       </Fragment>
     );
@@ -169,5 +168,4 @@ export default class EmailRecipients extends React.Component {
 EmailRecipients.propTypes = {
   httpClient: PropTypes.object.isRequired,
   type: PropTypes.string.isRequired,
-  notifications: PropTypes.object.isRequired,
 };
