570aaccf7bb43cffe3b181063ea74ff421d5fa06
Annie <leeyun@amazon.com>
2021-05-27 12:42:58 -0700

    Showing new metric popover

--- public/pages/CreateMonitor/components/MonitorExpressions/expressions/MetricExpression.js
+++ public/pages/CreateMonitor/components/MonitorExpressions/expressions/MetricExpression.js
@@ -1,5 +1,5 @@
 /*
- *   Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ *   Copyright 2021 Amazon.com, Inc. or its affiliates. All Rights Reserved.
  *
  *   Licensed under the Apache License, Version 2.0 (the "License").
  *   You may not use this file except in compliance with the License.
@@ -16,10 +16,25 @@
 import React, { Component } from 'react';
 import { connect } from 'formik';
 
-import { EuiPopover, EuiExpression, EuiButtonEmpty } from '@elastic/eui';
-import { Expressions, POPOVER_STYLE, AGGREGATION_TYPES, EXPRESSION_STYLE } from './utils/constants';
-import { selectOptionValueToText } from './utils/helpers';
-import { FormikSelect } from '../../../../../components/FormControls';
+import {
+  EuiText,
+  EuiPopover,
+  EuiExpression,
+  EuiButtonEmpty,
+  EuiFlexGroup,
+  EuiFlexItem,
+} from '@elastic/eui';
+import {
+  Expressions,
+  POPOVER_STYLE,
+  AGGREGATION_TYPES,
+  EXPRESSION_STYLE,
+  OVER_TYPES,
+} from './utils/constants';
+import { FormikComboBox, FormikSelect } from '../../../../../components/FormControls';
+import { getIndexFields } from './utils/dataTypes';
+import { getOfExpressionAllowedTypes } from './utils/helpers';
+import _ from 'lodash';
 
 class MetricExpression extends Component {
   onChangeWrapper = (e, field) => {
@@ -27,15 +42,57 @@ class MetricExpression extends Component {
     field.onChange(e);
   };
 
-  renderPopover = () => (
-    <div style={{ width: 180, ...POPOVER_STYLE, ...EXPRESSION_STYLE }}>
-      <FormikSelect
-        name="aggregationType"
-        inputProps={{
-          onChange: this.onChangeWrapper,
-          options: AGGREGATION_TYPES,
-        }}
-      />
+  onChangeFieldWrapper = (options, field, form) => {
+    this.props.onMadeChanges();
+    form.setFieldValue('fieldName', options);
+  };
+
+  renderPopover = (fieldOptions, expressionWidth) => (
+    <div style={{ width: Math.max(expressionWidth, 180), ...POPOVER_STYLE, ...EXPRESSION_STYLE }}>
+      <EuiFlexGroup direction="column" gutterSize="xs">
+        <EuiFlexItem>
+          <EuiText size="xs">
+            <h4>Aggregation</h4>
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          <FormikSelect
+            name="aggregationType"
+            inputProps={{
+              onChange: this.onChangeWrapper,
+              options: AGGREGATION_TYPES,
+            }}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
+
+      <EuiFlexGroup direction="column" gutterSize="xs">
+        <EuiFlexItem>
+          <EuiText size="xs">
+            <h4>Field</h4>
+          </EuiText>
+        </EuiFlexItem>
+        <EuiFlexItem>
+          {/*<FormikSelect*/}
+          {/*  name="aggregationField"*/}
+          {/*  inputProps={{*/}
+          {/*    onChange: this.onChangeWrapper,*/}
+          {/*    options: OVER_TYPES,*/}
+          {/*  }}*/}
+          {/*/>*/}
+          <FormikComboBox
+            name="fieldName"
+            inputProps={{
+              placeholder: 'Select a field',
+              options: fieldOptions,
+              onChange: this.onChangeFieldWrapper,
+              isClearable: false,
+              singleSelection: { asPlainText: true },
+              'data-test-subj': 'ofFieldComboBox',
+            }}
+          />
+        </EuiFlexItem>
+      </EuiFlexGroup>
     </div>
   );
 
@@ -45,35 +102,49 @@ class MetricExpression extends Component {
       openedStates,
       closeExpression,
       openExpression,
+      dataTypes,
     } = this.props;
+    //TODO: Generate correct fields options
+
+    const fieldOptions = getIndexFields(dataTypes, getOfExpressionAllowedTypes(values));
+    // const fieldOptions = [];
+    const expressionWidth =
+      Math.max(
+        ...fieldOptions.map(({ options }) =>
+          options.reduce((accu, curr) => Math.max(accu, curr.label.length), 0)
+        )
+      ) *
+        8 +
+      60;
     return (
-      <EuiPopover
-        id="metric-popover"
-        button={
-          <div>
-            <EuiExpression
-              description="metric"
-              value={selectOptionValueToText(values.aggregationType, AGGREGATION_TYPES)}
-              isActive={openedStates.WHEN}
-            />
-            <EuiButtonEmpty
-              size="xs"
-              onClick={() => openExpression(Expressions.WHEN)}
-              data-test-subj="addMetricButton"
-            >
-              + Add metric
-            </EuiButtonEmpty>
-          </div>
-        }
-        isOpen={openedStates.WHEN}
-        closePopover={() => closeExpression(Expressions.WHEN)}
-        panelPaddingSize="none"
-        ownFocus
-        withTitle
-        anchorPosition="downLeft"
-      >
-        {this.renderPopover()}
-      </EuiPopover>
+      <div>
+        <EuiText size="xs">
+          <h4>Metrics</h4>
+        </EuiText>
+        {/*TODO:Add badges here*/}
+        <EuiPopover
+          id="metric-popover"
+          button={
+            <div>
+              <EuiButtonEmpty
+                size="xs"
+                onClick={() => openExpression(Expressions.METRICS)}
+                data-test-subj="addMetricButton"
+              >
+                + Add metric
+              </EuiButtonEmpty>
+            </div>
+          }
+          isOpen={openedStates.METRICS}
+          closePopover={() => closeExpression(Expressions.METRICS)}
+          panelPaddingSize="none"
+          ownFocus
+          withTitle
+          anchorPosition="downLeft"
+        >
+          {this.renderPopover(fieldOptions, expressionWidth)}
+        </EuiPopover>
+      </div>
     );
   }
 }
